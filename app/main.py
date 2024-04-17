95fe0f8b88a20f925b5d6a3b9c41bb5d83b50dbb
FranciscoLozCoding <francisco.lozano33@outlook.com>
2024-04-17 15:11:54 -0500

    had wrong var name

--- app/main.py
+++ app/main.py
@@ -24,7 +24,7 @@ def main():
 
     gw = Gateway(name="wes-gateway", gateway_id="D2CE19FFFEC9D449", tenant_id="52f14cd4-c6f1-4fbd-8f87-4025e1d49242")
 
-    chirpstack_client.create_gateway(gw)
+    client.create_gateway(gw)
 
 if __name__ == "__main__":
     main() 
